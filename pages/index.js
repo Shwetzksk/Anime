84731544668cde093cdc8efc9625d15734a478eb
Shweta Kumari <shweta.kumari@zybisys.com>
2022-07-07 18:54:32 +0530

    Added getStaticProps in home page as meta data are getting aaded during nuild time

--- pages/index.js
+++ pages/index.js
@@ -6,11 +6,10 @@ import useFetch from "../hooks/useFetch";
 import Card from "../features/components/card/card";
 import { anime_list } from "../features/utility/api.util";
 
-export default function Home() {
+export default function Home(props) {
+  const { title, description, image_url, web_url } = props.data;
   const [page, setPage] = useState(1);
   const { isLoading, isError, res } = useFetch(anime_list(page));
-
-  console.log(res);
 
   if (isLoading) {
     return (
@@ -24,44 +23,26 @@ export default function Home() {
     <div>
       <Head>
         <title>Anime</title>
-        <meta
-          name="description"
-          content="Search or discover new anime content. This project is built using Jikan API. Developed by Shweta"
-        />
-        <meta name="title" content="Anime" />
+        <meta name="description" content={description} />
+        <meta name="title" content={title} />
         <meta name="robots" content="follow, index" />
 
         {/*----- facebook --------*/}
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://anime-shwetzksk.vercel.app" />
-        <link rel="canonical" href="https://anime-shwetzksk.vercel.app"></link>
-        <meta property="og:site_name" content="Anime"></meta>
-        <meta property="og:title" content="Anime" />
-        <meta
-          property="og:description"
-          content="Search or discover new anime content. This project is built using Jikan API. Developed by Shweta"
-        />
-        <meta
-          property="og:image"
-          content="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRl8o879xl4CXk3CvZccyFew50pvVsWyrxJ2Q&usqp=CAU"
-        />
+        <meta property="og:url" content={web_url} />
+        <link rel="canonical" href={web_url}></link>
+        <meta property="og:site_name" content={title}></meta>
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={image_url} />
 
         {/*-------- twitter -----------*/}
         <meta property="twitter:card" content="summary_large_image" />
-        <meta
-          property="twitter:url"
-          content="https://anime-shwetzksk.vercel.app"
-        />
+        <meta property="twitter:url" content={web_url} />
         <meta name="twitter:site" content="@shwetzksk"></meta>
-        <meta property="twitter:title" content="Anime" />
-        <meta
-          property="twitter:description"
-          content="Search or discover new anime content. This project is built using Jikan API. Developed by Shweta"
-        />
-        <meta
-          property="twitter:image"
-          content="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRl8o879xl4CXk3CvZccyFew50pvVsWyrxJ2Q&usqp=CAU"
-        />
+        <meta property="twitter:title" content={title} />
+        <meta property="twitter:description" content={description} />
+        <meta property="twitter:image" content={image_url} />
       </Head>
 
       <main className="flex flex-wrap">
@@ -71,4 +52,17 @@ export default function Home() {
       </main>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const data = {
+    title: "Anime",
+    description:
+      "Search or discover new anime content. This project is built using Jikan API. Developed by Shweta",
+    image_url:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRl8o879xl4CXk3CvZccyFew50pvVsWyrxJ2Q&usqp=CAU",
+    web_url: "https://anime-shwetzksk.vercel.app/",
+  };
+
+  return { props: { data } };
 }
