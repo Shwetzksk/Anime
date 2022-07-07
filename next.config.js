8f12adba8e2f19cd52be0ac4003dc92e81d11aea
Shweta Kumari <shweta.kumari@zybisys.com>
2022-07-07 17:31:17 +0530

    Testing metadata 2

--- next.config.js
+++ next.config.js
@@ -5,17 +5,17 @@ const nextConfig = {
     domains: ["cdn.myanimelist.net"],
     unoptimized: true,
   },
-  exportPathMap: async function (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
-    return {
-      "/": { page: "/" },
-      "/anime/1": { page: "/anime", query: { id: "1" } },
-      "/anime/5": { page: "/anime", query: { id: "5" } },
-      "/anime/6": { page: "/anime", query: { id: "6" } },
-    };
-  },
+  // exportPathMap: async function (
+  //   defaultPathMap,
+  //   { dev, dir, outDir, distDir, buildId }
+  // ) {
+  //   return {
+  //     "/": { page: "/" },
+  //     "/anime/1": { page: "/anime", query: { id: "1" } },
+  //     "/anime/5": { page: "/anime", query: { id: "5" } },
+  //     "/anime/6": { page: "/anime", query: { id: "6" } },
+  //   };
+  // },
 };
 
 module.exports = nextConfig;
