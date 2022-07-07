f10e28ceb7818946d5156d4f597703dcf2d2d2dc
Shweta Kumari <shweta.kumari@zybisys.com>
2022-07-07 17:18:01 +0530

    Testing metadata

--- next.config.js
+++ next.config.js
@@ -3,6 +3,18 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["cdn.myanimelist.net"],
+    unoptimized: true,
+  },
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      "/": { page: "/" },
+      "/anime/1": { page: "/anime", query: { id: "1" } },
+      "/anime/5": { page: "/anime", query: { id: "5" } },
+      "/anime/6": { page: "/anime", query: { id: "6" } },
+    };
   },
 };
 
